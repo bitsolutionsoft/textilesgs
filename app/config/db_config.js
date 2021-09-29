607f57c05b5931cefcaec944e42df208d107562c
unknown <jrenoj62@gmail.com>
2021-09-29 12:39:45 -0600

    apirest

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -1,12 +1,12 @@
 module.exports={
-    HOST:"localhost",
+    /* HOST:"localhost",
     USER:"root",
     PORT:"3306",
     PASSWORD:"root12345",
-    DB:"textilesgs",
-  /*  HOST:"bb109fjt3gmv0s8kc7ev-mysql.services.clever-cloud.com",
+    DB:"textilesgs",*/
+   HOST:"bb109fjt3gmv0s8kc7ev-mysql.services.clever-cloud.com",
     DB:"bb109fjt3gmv0s8kc7ev",
     USER:"u3rip7fn4nxcgrxj",   
     PASSWORD:"lU6aAQrBwIUFoAELzcXk",
-    PORT:"3306",*/
+    PORT:"3306",
 }
